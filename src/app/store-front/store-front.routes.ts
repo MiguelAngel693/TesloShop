677e23be1daf774b3ed9e580136becd0a92237c2
MiguelAngel693 <miguelag_2002@hotmail.com>
2026-09-19 00:20:41 -0300

    implementacion de rutas

---
+++ src/app/store-front/store-front.routes.ts
@@ -0,0 +1,38 @@
+import { Routes } from "@angular/router";
+import { StoreFrontLayout } from "./layouts/store-front-layout/store-front-layout";
+import { Home } from "./pages/home-page/home";
+import { Gender } from "./pages/gender-page/gender-page";
+import { Product } from "./pages/product/product";
+import { NotFound } from "./pages/not-found/not-found";
+
+export const storeFrontRoutes: Routes = [
+  {
+    path: '',
+    component: StoreFrontLayout,
+    children: [
+      {
+        path: '',
+        component: Home
+      },
+      {
+        path: 'gender/:gender',
+        component: Gender
+      },
+      {
+        path: 'product/:idSlug',
+        component: Product
+      },
+      {
+        path: '**',
+        component: NotFound,
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  }
+]
+
+
+export default storeFrontRoutes;
