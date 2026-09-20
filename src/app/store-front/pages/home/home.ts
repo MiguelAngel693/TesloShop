d7616d8be805d75d3769396c23e5b0fb69c83730
MiguelAngel693 <miguelag_2002@hotmail.com>
2026-09-19 21:42:56 -0300

    httpclient y servicio de productos

--- src/app/store-front/pages/home/home.ts
+++ src/app/store-front/pages/home/home.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, inject } from '@angular/core';
 import { ProductCard } from '../../components/product-card/product-card';
+import { Product } from '@/products/services/product';
+import { rxResource } from '@angular/core/rxjs-interop';
+
 
 @Component({
   selector: 'home',
   imports: [ProductCard],
   templateUrl: './home.html',
 })
-export class Home {}
+export class Home implements AfterViewInit{
+  private productService = inject(Product);
+
+  ngAfterViewInit(){
+    console.log('214');
+
+  }
+
+  productResource = rxResource({
+    params: () => ({}),
+    stream: ({ params }) => (
+      this.productService.getProducts()
+    )
+
+  })
+
+}
