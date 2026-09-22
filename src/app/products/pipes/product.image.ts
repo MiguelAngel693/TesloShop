9bfb988a45b27e353fcaf9bec6fbafdfc471f513
MiguelAngel693 <miguelag_2002@hotmail.com>
2026-09-21 21:43:35 -0300

    corregido baseUrl

--- src/app/products/pipes/product.image.ts
+++ src/app/products/pipes/product.image.ts
@@ -1,6 +1,8 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
+const baseUrl = environment.baseUrl+'/files/product';
+
 @Pipe({
   name: 'image'
 })
@@ -8,11 +10,11 @@ import { environment } from 'src/environments/environment';
 export class NamePipe implements PipeTransform {
   transform(url: string | string[]): string {
     if(typeof url === 'string'){
-      return environment.baseUrl+url;
+      return baseUrl+'/'+url;
     }
 
     if(url.length > 0){
-      return environment.baseUrl+url[0];
+      return baseUrl+'/'+url[0];
     }
 
     return './public/static/images/no-image.jpg';
