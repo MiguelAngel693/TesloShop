4829501eca84a64fe07f43b1bd6738d57d48d2f8
MiguelAngel693 <miguelag_2002@hotmail.com>
2026-09-24 12:40:05 -0300

    implementada pagina de productos por genero

--- src/app/store-front/pages/gender/gender-page.ts
+++ src/app/store-front/pages/gender/gender-page.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
-
+import { ProductService } from '@/products/services/product';
+import { Component, inject } from '@angular/core';
+import { toSignal, rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
+import { ProductCard } from '@/products/components/product-card/product-card';
 @Component({
   selector: 'gender',
-  imports: [],
+  imports: [ProductCard],
   templateUrl: './gender.html',
 })
-export class Gender {}
+export class Gender {
+  route = inject(ActivatedRoute);
+  productService = inject(ProductService);
+
+  productResource = rxResource({
+    params: () => ({gender: this.gender()}),
+    stream: ({params}) => (this.productService.getProducts({gender: params.gender}))
+  });
+
+  gender = toSignal(
+    this.route.params.pipe(
+      map(({ gender }) => gender)
+    )
+  );
+
+}
